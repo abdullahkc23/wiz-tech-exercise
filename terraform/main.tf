bd74840106af42525ca14981e0546832bece9f5b
abdullahkc23 <abdullah@khan-cheema.com>
2025-06-16 18:18:09 -0500

    Undid changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,14 +137,31 @@ resource "random_id" "bucket_id" {
 resource "aws_s3_bucket" "public_backups" {
   bucket        = "wiz-backups-${random_id.bucket_id.hex}"
   force_destroy = true
-  acl           = "public-read"
 }
 
 # Allow public access by disabling block settings
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket                  = aws_s3_bucket.public_backups.id
   block_public_acls       = false
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+# S3 is Public Read
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.public_backups.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicRead",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.public_backups.arn}/*"
+      }
+    ]
+  })
 }
