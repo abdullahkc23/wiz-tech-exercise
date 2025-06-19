20efcce99b94368c722c0fef543a2df42292eb5d
abdullahkc23 <abdullah@khan-cheema.com>
2025-06-19 16:08:38 -0500

    Uses indexing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,31 +1,31 @@
 output "instance_public_ip" {
   description = "Public IP of the MongoDB instance"
-  value       = var.create_ec2 ? aws_instance.mongo.public_ip : null
+  value       = var.create_ec2 ? aws_instance.mongo[0].public_ip : null
 }
 
 output "s3_bucket_name" {
   description = "Name of the public S3 backup bucket"
-  value       = var.create_s3 ? aws_s3_bucket.public_backups.id : null
+  value       = var.create_s3 ? aws_s3_bucket.public_backups[0].id : null
 }
 
 output "mongodb_status_url" {
   description = "Public URL to check MongoDB version and backup timestamp"
-  value       = var.create_ec2 ? "http://${aws_instance.mongo.public_ip}/status.txt" : null
+  value       = var.create_ec2 ? "http://${aws_instance.mongo[0].public_ip}/status.txt" : null
 }
 
 output "mongo_ami_id" {
   description = "The AMI ID used for the MongoDB instance"
-  value       = var.create_ec2 ? aws_instance.mongo.ami : null
+  value       = var.create_ec2 ? aws_instance.mongo[0].ami : null
 }
 
 output "mongo_instance_state" {
   description = "State of the MongoDB instance"
-  value       = var.create_ec2 ? aws_instance.mongo.instance_state : null
+  value       = var.create_ec2 ? aws_instance.mongo[0].instance_state : null
 }
 
 output "s3_bucket_arn" {
   description = "ARN of the S3 backup bucket"
-  value       = var.create_s3 ? aws_s3_bucket.public_backups.arn : null
+  value       = var.create_s3 ? aws_s3_bucket.public_backups[0].arn : null
 }
 
 output "mongo_version" {
@@ -33,12 +33,12 @@ output "mongo_version" {
   value       = "3.6.23"
 }
 
-# Expose the most recent backup file in S3
-output "latest_backup_file" {
-  description = "Latest MongoDB backup file in S3"
-  value       = (
-    var.create_s3 && length(data.aws_s3_bucket_object.latest_backup.*.key) > 0
-    ? data.aws_s3_bucket_object.latest_backup[0].key
-    : "No backups found"
-  )
-}
+# Uncomment this section only if you add the corresponding data block
+# output "latest_backup_file" {
+#   description = "Latest MongoDB backup file in S3"
+#   value       = (
+#     var.create_s3 && length(data.aws_s3_bucket_object.latest_backup.*.key) > 0
+#     ? data.aws_s3_bucket_object.latest_backup[0].key
+#     : "No backups found"
+#   )
+# }
