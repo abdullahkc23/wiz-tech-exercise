d5c368db0650808ebd83f326bafcde61cd312c35
abdullahkc23 <abdullah@khan-cheema.com>
2025-06-19 11:56:11 -0500

    Introduce variables for EC2, S3, IAM to avoid replication

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,21 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "create_ec2" {
+  description = "Set to true to create EC2 instance"
+  type        = bool
+  default     = true
+}
+
+variable "create_s3" {
+  description = "Set to true to create S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "create_iam" {
+  description = "Set to true to create IAM role and instance profile"
+  type        = bool
+  default     = true
+}
