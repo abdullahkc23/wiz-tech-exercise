40c16b825a2f9435ab6d8b29a32251ee6e75ee54
abdullahkc23 <abdullah@khan-cheema.com>
2025-06-19 23:35:16 -0500

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -76,7 +76,7 @@ resource "aws_eks_node_group" "wiz_nodes" {
   cluster_name    = aws_eks_cluster.wiz_eks[0].name
   node_group_name = "wiz-eks-nodes-v3"
   node_role_arn   = aws_iam_role.eks_node_role[0].arn
-  subnet_ids      = [aws_subnet.public.id]
+  subnet_ids      = [aws_subnet.public_a.id]
 
   scaling_config {
     desired_size = 1
