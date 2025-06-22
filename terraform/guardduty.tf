54a3c8cbf3274e74f5c85d623a1c8da40a99772a
abdullahkc23 <abdullah@khan-cheema.com>
2025-06-21 19:29:30 -0500

    Update guardduty

--- terraform/guardduty.tf
+++ terraform/guardduty.tf
@@ -1,10 +1,3 @@
 resource "aws_guardduty_detector" "main" {
   enable = true
 }
-
-resource "aws_guardduty_member" "self" {
-  detector_id          = aws_guardduty_detector.main.id
-  account_id           = var.account_id
-  email                = var.account_email
-  invite               = false
-}
